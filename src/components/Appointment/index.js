8044c1b0cb7e11a0c9bab7f3748efc537a14b248
Edward Ascan <edward.ascan@Edwards-MacBook-Air.local>
2021-02-17 18:13:27 -0500

    add show to appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, {Fragment} from "react";
 import "components/Appointment/index.scss";
 import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
 
 export default function Appointment(props) {
-  return <article className="appointment"></article>;
+  return (
+    <div>
+      <Header time={props.time} />
+      {props.interview ? (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      ) : (
+        <Empty />
+      )}
+    </div>
+  );
 }
